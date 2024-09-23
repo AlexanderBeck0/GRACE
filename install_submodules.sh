dfc14f3840e971f597cefa912aa7aab0fbe476d2
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-09-23 17:20:15 -0400

    Added a bash file for installing submodules

---
+++ install_submodules.sh
@@ -0,0 +1,4 @@
+# bash install_submodules.sh
+cd src
+git clone --recursive https://github.com/leggedrobotics/darknet_ros -o src
+git clone --recursive https://github.com/ipa320/ipa_coverage_planning -o src

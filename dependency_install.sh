dc8566a326052f397fc000f3887f8b770377fe43
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-09-24 17:18:21 -0400

    Added dependency install script

---
+++ dependency_install.sh
@@ -0,0 +1,6 @@
+cd ../
+# https://github.com/ipa320/ipa_coverage_planning/issues/8#issuecomment-1971892059
+sudo apt-get install libopencv-dev libboost-all-dev coinor-libcoinutils-dev coinor-libosi-dev coinor-libclp-dev coinor-libcgl-dev coinor-libcbc-dev
+sudo apt install ros-melodic-cob-map-accessibility-analysis
+sudo apt install ros-melodic-opengm
+sudo apt install ros-melodic-libdlib

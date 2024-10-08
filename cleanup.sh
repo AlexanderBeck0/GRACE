700fbe637dc1b20fdc046f61874b8e817d52dabb
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-10-08 16:35:18 -0400

    Got yolo working

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
-rm /out/Detections/*.txt
-rm /out/Images/*.png
-rm /out/MissionStatus/*.txt
-rm /out/RoomLabels/*.txt
-rm /out/Reward/*.jpg 
-rm /out/SLAM/*.txt
+rm out/Detections/*.txt
+rm out/Images/*.png
+rm out/MissionStatus/*.txt
+rm out/RoomLabels/*.txt
+rm out/Reward/*.jpg 
+rm out/SLAM/*.txt
